60a225fff5b8043a17af9d82abd383de9a55a291
Suraj Verma <surajverma@rentalcars.com>
2020-09-06 11:33:00 +0100

    add react-boostrap and boostrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Button />
+    </div>
+  );
 };
 export default App;
